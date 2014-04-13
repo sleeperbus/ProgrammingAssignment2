45d300e09edb5e00206bd53693005483cf44410f
Hojoon Ji <sleeperbus@gmail.com>
2014-04-13 23:23:22 +0900

    first save

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+###############################################################################
+#   Calculate inverse matrix and cache the result for reusablity
+###############################################################################
 
-## Write a short comment describing this function
 
+## Initialize orgMat variable to x as soon as this object is created.
 makeCacheMatrix <- function(x = matrix()) {
-
+    orgMat <- NULL
+    inverseMat <- NULL
+    setMat <- function(mat) {
+        orgMat <<- mat
+        inverseMat <<- NULL
+    }
+    getMat <- function() orgMat
+    setInverseMat <- function(mat) inverseMat <<- mat
+    getInverseMat <- function() inverseMat
+    setMat(x)
+    list(setMat = setMat, getMat = getMat, setInverseMat = setInverseMat,
+         getInverseMat = getInverseMat)    
 }
 
-
-## Write a short comment describing this function
-
+# return cache data if exists
+# otherwise use solve function to calculate inverse matrix and save to
+# caller object
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverseMat <- x$getInverseMat()
+    if (!is.null(inverseMat)) {
+        message("getting from cache")
+        return (inverseMat)
+    }
+    x$setInverseMat(solve(x$getMat()))
+    inverseMat <- x$getInverseMat()
+    inverseMat
 }
